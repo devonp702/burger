c709c0ce41aa50f26615660a3524ccf095b4742a
Blackjack2020 <69491789+devonp702@users.noreply.github.com>
2020-11-02 16:02:41 -0700

    Added some psudocoding, setting up the orm

--- config/orm.js
+++ config/orm.js
@@ -6,33 +6,26 @@ var con = require("./connection.js");
 // These help avoid SQL injection
 // https://en.wikipedia.org/wiki/SQL_injection
 var orm = {
-  selectWhere: function(tableInput, colToSearch, valOfCol) {
-    var queryString = "SELECT * FROM ?? WHERE ?? = ?";
+  selectAll: function(tableInput, colToSearch, valOfCol) {
+    var queryString = "SELECT * FROM ??";
+    con.query(queryString, [tableInput], function(err, result) {
+      if (err) throw err;
+      console.log(result);
+    });
+  },
+  insertOne: function(tableInput, colToSearch, valOfCol) {
+    var queryString = "INSERT INTO ?? (??) VALUES (?)";
     con.query(queryString, [tableInput, colToSearch, valOfCol], function(err, result) {
       if (err) throw err;
       console.log(result);
     });
   },
-  selectAndOrder: function(whatToSelect, table, orderCol) {
-    var queryString = "SELECT ?? FROM ?? ORDER BY ?? DESC";
-    console.log(queryString);
-    con.query(queryString, [whatToSelect, table, orderCol], function(err, result) {
+  updateOne: function(tableInput, colToSearch, valOfCol) {
+    var queryString = "SELECT * FROM ?? WHERE ?? = ?";
+    con.query(queryString, [tableInput, colToSearch, valOfCol], function(err, result) {
       if (err) throw err;
       console.log(result);
     });
-  },
-  findWhoHasMost: function(tableOneCol, tableTwoForeignKey, tableOne, tableTwo) {
-    var queryString =
-      "SELECT ??, COUNT(??) AS count FROM ?? LEFT JOIN ?? ON ??.??= ??.id GROUP BY ?? ORDER BY count DESC LIMIT 1";
-
-    con.query(
-      queryString,
-      [tableOneCol, tableOneCol, tableOne, tableTwo, tableTwo, tableTwoForeignKey, tableOne, tableOneCol],
-      function(err, result) {
-        if (err) throw err;
-        console.log(result);
-      }
-    );
   }
 };
 
