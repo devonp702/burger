9456568f7e7bf720f979339cfcefe53e8e713b2c
Blackjack2020 <69491789+devonp702@users.noreply.github.com>
2020-11-22 20:56:28 -0700

    Fixed some bugs, added js file

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,21 @@
 var mysql = require('mysql');
 
-var con = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "burgers_db"
-});
+var con;
+if (process.env.JAWSDB_URL) {
+  con = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  con = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "burgers_db"
+  });
+}
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+  con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
 
-module.exports = con;
+  module.exports = con;
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -12,16 +12,18 @@ router.get("/", function(req, res) {
     res.render("index", expObj);
   });
 });
-router.post("/api/burgers", function(req, res) {
-  burger.create([
-    "burger_name", ""
-  ], [
-    req.body.burger_name, req.body.devoured
-  ], function(result) {
-    // Send back the ID of the new quote
-    res.json({ id: result.insertId });
-  });
+
+router.post("/api/burgers", function (req, res) {
+  burger.create(
+    ["burger_name", "devoured"],
+    [req.body.burger_name, req.body.devour],
+    function (result) {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
+    }
+  );
 });
+
 
 router.put("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
@@ -29,7 +31,7 @@ router.put("/api/burgers/:id", function(req, res) {
   console.log("condition", condition);
 
   burger.update({
-    devoured: req.body.devoured
+    devoured: 1,
   }, condition, function(result) {
     if (result.changedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
